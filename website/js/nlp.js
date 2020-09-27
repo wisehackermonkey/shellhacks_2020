43ea57597c40a050669d42f46963ce15757adb39
Oran C <Oranbusiness@gmail.com>
2020-09-26 22:34:27 -0700

    [fixed] server - client code

--- website/js/nlp.js
+++ website/js/nlp.js
@@ -2,10 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
+const cors = require("cors")
 // const JSON = require("json")
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Creates a client
+const client = new language.LanguageServiceClient();
 
 const port = 5000
 //http://localhost:4000/api/nlp
@@ -27,26 +34,18 @@ app.listen(port, () => {
 
 console.log("HI")
 //quickstart();
-async function quickstart(text) {
-// Imports the Google Cloud client library
-const language = require('@google-cloud/language');
-
-// Creates a client
-const client = new language.LanguageServiceClient();
-
+function quickstart(text) {
 /**
  * TODO(developer): Uncomment the following line to run this code.
  */
 //const text = 'apples are yummy and bananas are tasty and food is good and i like pears and fruits and oranges and pineapples and grapes and watermelon and melons!';
 
 // Prepares a document, representing the provided text
-var document = {
-  content: text,
-  type: 'PLAIN_TEXT',
-};
+// var document = 
 
 // Classifies text in the document
 async function googleAnalyze(document) {
+  
 const [classification] = await client.classifyText({document});
 console.log('Categories:');
 classification.categories.forEach(category => {
@@ -54,8 +53,14 @@ classification.categories.forEach(category => {
 });
 return classification.categories
 }
+document = {
+  content: text,
+  type: 'PLAIN_TEXT',
+}
+googleAnalyze(document) .then(data => console.log(data))
+.catch(reason => console.log(reason.message))
 
-googleAnalyze(document);
+
 }
 
 
